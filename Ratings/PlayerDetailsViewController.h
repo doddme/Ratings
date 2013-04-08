945d521271007c5655c89c5e6753d5fee7a4bfa8
Michael Dodd <doddme@Michaels-MacBook-Pro.local>
2013-04-08 16:27:51 -0500

    Lots of unsynced commits
    Last one was added delegate back to first

--- Ratings/PlayerDetailsViewController.h
+++ Ratings/PlayerDetailsViewController.h
@@ -13,11 +13,13 @@
 -(void) playerDetailsViewControllerDidCancel:(PlayerDetailsViewController *) controller;
 -(void) playerDetailsViewControllerDidSave:(PlayerDetailsViewController *) controller;
 
-
 @end
 
 @interface PlayerDetailsViewController : UITableViewController
 @property (nonatomic,weak) id <PlayerDetailsViewControllerDelegate> delegate;
 -(IBAction)cancel:(id)sender;
 -(IBAction)done:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UILabel *detailLabel;
+
 @end
